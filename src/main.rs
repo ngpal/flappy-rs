56434dfc1c48620aa0b688871d0d8f6e6c529d72
Nandagopal Menon <nandagopalnmenon@gmail.com>
2025-02-05 20:08:18 +0530

    Constants

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,17 @@ const WINDOW_HEIGHT: f32 = 500.;
 
 const BIRD_RADIUS: f32 = 20.;
 const BIRD_FLAP: f32 = 9.;
+const BIRD_START: f32 = 100.;
 
-const HORIZONTAL_SPEED: f32 = 5.;
+const FLY_GAP: f32 = 150.;
+
+const SCROLL_SPEED: f32 = 5.;
+
+const PIPE_START: f32 = 600.;
+const PIPE_SEP: f32 = 300.;
+const PIPE_WIDTH: f32 = 50.;
+const PIPE_HEIGHT: f32 = 400.;
+const PIPE_BASE_Y: f32 = (PIPE_HEIGHT / 2.) + (FLY_GAP / 2.);
 
 #[derive(Component, Default)]
 struct Bird {
@@ -46,37 +55,37 @@ fn setup(
         Bird::default(),
         Mesh2d(meshes.add(Circle::new(BIRD_RADIUS))),
         MeshMaterial2d(materials.add(Color::srgb(1., 0., 1.))),
-        Transform::from_xyz(0.0, 100.0, 1.0),
+        Transform::from_xyz(0., BIRD_START, 1.),
     ));
 
     // First set
     commands.spawn((
         Pipe::default(),
-        Mesh2d(meshes.add(Rectangle::new(50., 400.))),
+        Mesh2d(meshes.add(Rectangle::new(PIPE_WIDTH, PIPE_HEIGHT))),
         MeshMaterial2d(materials.add(Color::srgb(1., 1., 1.))),
-        Transform::from_xyz(600., 275., 0.),
+        Transform::from_xyz(PIPE_START, PIPE_BASE_Y, 0.),
     ));
 
     commands.spawn((
         Pipe::default(),
-        Mesh2d(meshes.add(Rectangle::new(50., 400.))),
+        Mesh2d(meshes.add(Rectangle::new(PIPE_WIDTH, PIPE_HEIGHT))),
         MeshMaterial2d(materials.add(Color::srgb(1., 1., 1.))),
-        Transform::from_xyz(600., -275., 0.),
+        Transform::from_xyz(PIPE_START, -PIPE_BASE_Y, 0.),
     ));
 
     // Second set
     commands.spawn((
         Pipe::default(),
-        Mesh2d(meshes.add(Rectangle::new(50., 400.))),
+        Mesh2d(meshes.add(Rectangle::new(PIPE_WIDTH, PIPE_HEIGHT))),
         MeshMaterial2d(materials.add(Color::srgb(1., 1., 1.))),
-        Transform::from_xyz(900., 275., 0.),
+        Transform::from_xyz(PIPE_START + PIPE_SEP, PIPE_BASE_Y, 0.),
     ));
 
     commands.spawn((
         Pipe::default(),
-        Mesh2d(meshes.add(Rectangle::new(50., 400.))),
+        Mesh2d(meshes.add(Rectangle::new(PIPE_WIDTH, PIPE_HEIGHT))),
         MeshMaterial2d(materials.add(Color::srgb(1., 1., 1.))),
-        Transform::from_xyz(900., -275., 0.),
+        Transform::from_xyz(PIPE_START + PIPE_SEP, -PIPE_BASE_Y, 0.),
     ));
 }
 
@@ -97,10 +106,10 @@ fn flap(mut query: Query<&mut Bird>, keys: Res<ButtonInput<KeyCode>>) {
 
 fn move_pipes(mut query: Query<&mut Transform, With<Pipe>>) {
     for mut transform in query.iter_mut() {
-        if transform.translation.x <= -300. {
-            transform.translation.x = 300.;
+        if transform.translation.x <= -PIPE_SEP {
+            transform.translation.x = PIPE_SEP;
         }
 
-        transform.translation.x -= HORIZONTAL_SPEED;
+        transform.translation.x -= SCROLL_SPEED;
     }
 }

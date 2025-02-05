8d40701d7c97c9c34ea9cc6c4096f2b9f8df89f4
Nandagopal Menon <nandagopalnmenon@gmail.com>
2025-02-05 19:56:15 +0530

    Moving pipes

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,18 @@ const WINDOW_WIDTH: f32 = 400.;
 const WINDOW_HEIGHT: f32 = 500.;
 
 const BIRD_RADIUS: f32 = 20.;
-const BIRD_FLAP: f32 = 10.;
+const BIRD_FLAP: f32 = 9.;
+
+const HORIZONTAL_SPEED: f32 = 5.;
 
 #[derive(Component, Default)]
 struct Bird {
     velocity: f32,
+}
+
+#[derive(Component, Default)]
+struct Pipe {
+    y_offset: f32,
 }
 
 fn main() {
@@ -25,7 +32,7 @@ fn main() {
             ..Default::default()
         }))
         .add_systems(Startup, setup)
-        .add_systems(Update, (gravity, flap).chain())
+        .add_systems(Update, (gravity, flap, move_pipes).chain())
         .run();
 }
 
@@ -41,6 +48,36 @@ fn setup(
         MeshMaterial2d(materials.add(Color::srgb(1., 0., 1.))),
         Transform::from_xyz(0.0, 100.0, 1.0),
     ));
+
+    // First set
+    commands.spawn((
+        Pipe::default(),
+        Mesh2d(meshes.add(Rectangle::new(50., 400.))),
+        MeshMaterial2d(materials.add(Color::srgb(1., 1., 1.))),
+        Transform::from_xyz(600., 275., 0.),
+    ));
+
+    commands.spawn((
+        Pipe::default(),
+        Mesh2d(meshes.add(Rectangle::new(50., 400.))),
+        MeshMaterial2d(materials.add(Color::srgb(1., 1., 1.))),
+        Transform::from_xyz(600., -275., 0.),
+    ));
+
+    // Second set
+    commands.spawn((
+        Pipe::default(),
+        Mesh2d(meshes.add(Rectangle::new(50., 400.))),
+        MeshMaterial2d(materials.add(Color::srgb(1., 1., 1.))),
+        Transform::from_xyz(900., 275., 0.),
+    ));
+
+    commands.spawn((
+        Pipe::default(),
+        Mesh2d(meshes.add(Rectangle::new(50., 400.))),
+        MeshMaterial2d(materials.add(Color::srgb(1., 1., 1.))),
+        Transform::from_xyz(900., -275., 0.),
+    ));
 }
 
 fn gravity(mut query: Query<(&mut Bird, &mut Transform)>) {
@@ -55,5 +92,15 @@ fn flap(mut query: Query<&mut Bird>, keys: Res<ButtonInput<KeyCode>>) {
     let mut bird = query.get_single_mut().unwrap();
     if keys.just_pressed(KeyCode::Space) {
         bird.velocity = BIRD_FLAP;
+    }
+}
+
+fn move_pipes(mut query: Query<&mut Transform, With<Pipe>>) {
+    for mut transform in query.iter_mut() {
+        if transform.translation.x <= -300. {
+            transform.translation.x = 300.;
+        }
+
+        transform.translation.x -= HORIZONTAL_SPEED;
     }
 }
